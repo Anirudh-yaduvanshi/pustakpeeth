416e10fd5a51cf124df83cd992997b17a50a63a6
Anirudh yadav <yadavanirudh97.25@gmail.com>
2025-06-16 10:47:24 +0530

    cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 const cors = require('cors');
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://your-production-site.com'], // Add production origin
+    origin: ['http://localhost:5173', 'https://pushtakmanadi.onrender.com/'], // Add production origin
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'], // Include PATCH method
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'auth-token'], // Add auth-token here
     exposedHeaders: ['Content-Length', 'X-Kuma-Revision', 'X-Custom-Header'], // Add a custom exposed header if needed
